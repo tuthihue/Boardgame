e5f70b23962b689f1c9556dd254c3d58c2f0bf43
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-06-27 23:29:01 +0700

    Thêm giao diện trang chủ của web

---
+++ BoardGame/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ | BoardGame</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+<div id="wrapper">
+        <div id="header">
+            <div class="container">
+                <div class="logo">
+                    <img src="logo-boardgame.png" alt="">
+                </div>
+                <div class="search-bar">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <input type="search" placeholder="Tìm kiếm">
+                </div>
+            </div>
+            <div id="menu">
+              <div class="container">
+              <div class="menu-area">
+                  <ul>
+                      <li><a href="/BoardGame/index.php">Trang chủ</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+          </div>
+      </div>
+
+        <!-- end header -->
+        
+         <!-- end foooter -->
+    </div>
+</body>
+
+</html>
