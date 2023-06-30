e90ff02c9cafec1587c6b81c0411914c878672bd
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-07-01 00:10:48 +0700

    Chức năng thêm sản phẩm

--- APP/APP/APP/ThongtinBoardGame.Designer.cs
+++ APP/APP/APP/ThongtinBoardGame.Designer.cs
@@ -35,9 +35,26 @@
             addBG = new TabPage();
             delBG = new TabPage();
             updateBG = new TabPage();
+            label4 = new Label();
+            AddAdmin = new Panel();
+            Mota = new TextBox();
+            label5 = new Label();
+            them = new Button();
+            SL = new TextBox();
+            email = new Label();
+            Theloai = new TextBox();
+            sdt = new Label();
+            Gia = new TextBox();
+            pass = new Label();
+            TenBG = new TextBox();
+            name = new Label();
+            MaBG = new TextBox();
+            label = new Label();
             tabControl1.SuspendLayout();
             TTSP.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)TTBoardGame).BeginInit();
+            addBG.SuspendLayout();
+            AddAdmin.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -92,6 +109,8 @@
             // 
             // addBG
             // 
+            addBG.Controls.Add(label4);
+            addBG.Controls.Add(AddAdmin);
             addBG.Location = new Point(4, 29);
             addBG.Name = "addBG";
             addBG.Padding = new Padding(3);
@@ -120,6 +139,145 @@
             updateBG.Text = "Sửa BoardGame";
             updateBG.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.HotTrack;
+            label4.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label4.ForeColor = Color.Cornsilk;
+            label4.Location = new Point(0, 3);
+            label4.Name = "label4";
+            label4.Size = new Size(283, 41);
+            label4.TabIndex = 12;
+            label4.Text = "Thêm BoardGame";
+            // 
+            // AddAdmin
+            // 
+            AddAdmin.Controls.Add(Mota);
+            AddAdmin.Controls.Add(label5);
+            AddAdmin.Controls.Add(them);
+            AddAdmin.Controls.Add(SL);
+            AddAdmin.Controls.Add(email);
+            AddAdmin.Controls.Add(Theloai);
+            AddAdmin.Controls.Add(sdt);
+            AddAdmin.Controls.Add(Gia);
+            AddAdmin.Controls.Add(pass);
+            AddAdmin.Controls.Add(TenBG);
+            AddAdmin.Controls.Add(name);
+            AddAdmin.Controls.Add(MaBG);
+            AddAdmin.Controls.Add(label);
+            AddAdmin.Location = new Point(193, 103);
+            AddAdmin.Name = "AddAdmin";
+            AddAdmin.Size = new Size(636, 443);
+            AddAdmin.TabIndex = 11;
+            // 
+            // Mota
+            // 
+            Mota.Location = new Point(133, 334);
+            Mota.Name = "Mota";
+            Mota.Size = new Size(268, 27);
+            Mota.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(47, 334);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Mô tả";
+            // 
+            // them
+            // 
+            them.BackColor = Color.Green;
+            them.Location = new Point(350, 385);
+            them.Name = "them";
+            them.Size = new Size(97, 38);
+            them.TabIndex = 10;
+            them.Text = "Thêm";
+            them.UseVisualStyleBackColor = false;
+            them.Click += them_Click;
+            // 
+            // SL
+            // 
+            SL.Location = new Point(133, 277);
+            SL.Name = "SL";
+            SL.Size = new Size(268, 27);
+            SL.TabIndex = 9;
+            // 
+            // email
+            // 
+            email.AutoSize = true;
+            email.Location = new Point(25, 277);
+            email.Name = "email";
+            email.Size = new Size(69, 20);
+            email.TabIndex = 8;
+            email.Text = "Số lượng";
+            // 
+            // Theloai
+            // 
+            Theloai.Location = new Point(133, 218);
+            Theloai.Name = "Theloai";
+            Theloai.Size = new Size(268, 27);
+            Theloai.TabIndex = 7;
+            // 
+            // sdt
+            // 
+            sdt.AutoSize = true;
+            sdt.Location = new Point(25, 221);
+            sdt.Name = "sdt";
+            sdt.Size = new Size(65, 20);
+            sdt.TabIndex = 6;
+            sdt.Text = "Thể Loại";
+            // 
+            // Gia
+            // 
+            Gia.Location = new Point(133, 158);
+            Gia.Name = "Gia";
+            Gia.Size = new Size(268, 27);
+            Gia.TabIndex = 5;
+            // 
+            // pass
+            // 
+            pass.AutoSize = true;
+            pass.Location = new Point(47, 158);
+            pass.Name = "pass";
+            pass.Size = new Size(31, 20);
+            pass.TabIndex = 4;
+            pass.Text = "Giá";
+            // 
+            // TenBG
+            // 
+            TenBG.Location = new Point(133, 99);
+            TenBG.Name = "TenBG";
+            TenBG.Size = new Size(268, 27);
+            TenBG.TabIndex = 3;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.Location = new Point(14, 99);
+            name.Name = "name";
+            name.Size = new Size(115, 20);
+            name.TabIndex = 2;
+            name.Text = "Tên BoardGame";
+            // 
+            // MaBG
+            // 
+            MaBG.Location = new Point(133, 42);
+            MaBG.Name = "MaBG";
+            MaBG.Size = new Size(268, 27);
+            MaBG.TabIndex = 1;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Location = new Point(14, 42);
+            label.Name = "label";
+            label.Size = new Size(113, 20);
+            label.TabIndex = 0;
+            label.Text = "Mã BoardGame";
+            // 
             // ThongtinBoardGame
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -132,6 +290,10 @@
             TTSP.ResumeLayout(false);
             TTSP.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)TTBoardGame).EndInit();
+            addBG.ResumeLayout(false);
+            addBG.PerformLayout();
+            AddAdmin.ResumeLayout(false);
+            AddAdmin.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -144,5 +306,20 @@
         private TabPage addBG;
         private TabPage delBG;
         private TabPage updateBG;
+        private Label label4;
+        private Panel AddAdmin;
+        private TextBox Mota;
+        private Label label5;
+        private Button them;
+        private TextBox SL;
+        private Label email;
+        private TextBox Theloai;
+        private Label sdt;
+        private TextBox Gia;
+        private Label pass;
+        private TextBox TenBG;
+        private Label name;
+        private TextBox MaBG;
+        private Label label;
     }
 }
--- APP/APP/APP/ThongtinBoardGame.cs
+++ APP/APP/APP/ThongtinBoardGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,54 @@ namespace APP
 {
     public partial class ThongtinBoardGame : Form
     {
+        static MySqlConnection conn;
         public ThongtinBoardGame()
         {
             InitializeComponent();
+            Connection();
+        }
+        static void Connection()
+        {
+            string connstr = "server=127.0.0.1;uid=root;pwd=;database=boardgame";
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void them_Click(object sender, EventArgs e)
+        {
+            string masp = MaBG.Text;
+            string tensp = TenBG.Text;
+            string gia = Gia.Text;
+            string TL = Theloai.Text;
+            string Soluong = SL.Text;
+            string mt = Mota.Text;
+            conn.Open();
+            MySqlCommand mySqlCommand = new MySqlCommand("insert into boardgame values(@masp,@tensp,@gia,@TL,@soluong,@mt) ", conn);
+            mySqlCommand.Parameters.AddWithValue("@masp", masp);
+            mySqlCommand.Parameters.AddWithValue("@tensp", tensp);
+            mySqlCommand.Parameters.AddWithValue("@gia", gia);
+            mySqlCommand.Parameters.AddWithValue("@TL", TL);
+            mySqlCommand.Parameters.AddWithValue("@Soluong", Soluong);
+            mySqlCommand.Parameters.AddWithValue("@mt", mt);
+            mySqlCommand.ExecuteNonQuery();
+            MessageBox.Show("Thêm BoardGame thành công!");
+            conn.Close();
+            MaBG.Text = "";
+            TenBG.Text = "";
+            Gia.Text = "";
+            Theloai.Text = "";
+            SL.Text = "";
+            Mota.Text = "";
         }
     }
 }
