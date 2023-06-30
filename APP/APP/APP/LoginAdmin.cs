91a6b3710fff9e04c6ca4e2fe1a39a49fea4cf13
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-06-30 23:39:22 +0700

    Kiểm tra đăng nhập và chuyển sang trang quản lí boardgame

--- APP/APP/APP/LoginAdmin.cs
+++ APP/APP/APP/LoginAdmin.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using MySql.Data.MySqlClient;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace APP
@@ -27,14 +27,51 @@ namespace APP
             }
             finally { conn.Close(); }
         }
+        void LoginValidation()
+        {
+            string uname = account.Text;
+            string pass = password.Text;
+            Account_Admin = uname;
 
+            try
+            {
+                conn.Open();
+                MySqlCommand mySqlComman = new MySqlCommand("select * from admins", conn);
+                MySqlDataReader Reader = mySqlComman.ExecuteReader();
+                while (Reader.Read())
+                {
+                    if (uname.Equals(Reader.GetString("USERNAME")) && pass.Equals(Reader.GetString("PassWord")))
+                    {
+                        QuanLiBoardGame ql = new QuanLiBoardGame(Account_Admin);
+                        this.Hide();
+                        ql.ShowDialog();
+                        this.Show();
+                        conn.Close();
+                        return;
+                    }
+
+                }
+                MessageBox.Show("Tài khoản hoặc mật khẩu sai!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+                return;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
         private void Login_Click(object sender, EventArgs e)
         {
-
+            LoginValidation();
         }
 
         private void Exit_Click(object sender, EventArgs e)
         {
+            Application.Exit();
         }
 
         private void username_Click(object sender, EventArgs e)
