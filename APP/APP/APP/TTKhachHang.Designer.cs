f6c3beb80151e3a091e244bb3c266c860fc4089d
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-06-30 23:54:23 +0700

    Chức năng thêm khách hàng

--- APP/APP/APP/TTKhachHang.Designer.cs
+++ APP/APP/APP/TTKhachHang.Designer.cs
@@ -35,9 +35,24 @@
             addKH = new TabPage();
             delKH = new TabPage();
             updateKH = new TabPage();
+            AddAdmin = new Panel();
+            them = new Button();
+            tkemail = new TextBox();
+            email = new Label();
+            sodienthoai = new TextBox();
+            sdt = new Label();
+            password = new TextBox();
+            pass = new Label();
+            hovaten = new TextBox();
+            name = new Label();
+            username = new TextBox();
+            uname = new Label();
+            label4 = new Label();
             TTKH.SuspendLayout();
             TTSP.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)TTCustomers).BeginInit();
+            addKH.SuspendLayout();
+            AddAdmin.SuspendLayout();
             SuspendLayout();
             // 
             // TTKH
@@ -93,6 +108,8 @@
             // 
             // addKH
             // 
+            addKH.Controls.Add(AddAdmin);
+            addKH.Controls.Add(label4);
             addKH.Location = new Point(4, 29);
             addKH.Name = "addKH";
             addKH.Padding = new Padding(3);
@@ -121,6 +138,127 @@
             updateKH.Text = "Sửa Thông tin khách hàng";
             updateKH.UseVisualStyleBackColor = true;
             // 
+            // AddAdmin
+            // 
+            AddAdmin.Controls.Add(them);
+            AddAdmin.Controls.Add(tkemail);
+            AddAdmin.Controls.Add(email);
+            AddAdmin.Controls.Add(sodienthoai);
+            AddAdmin.Controls.Add(sdt);
+            AddAdmin.Controls.Add(password);
+            AddAdmin.Controls.Add(pass);
+            AddAdmin.Controls.Add(hovaten);
+            AddAdmin.Controls.Add(name);
+            AddAdmin.Controls.Add(username);
+            AddAdmin.Controls.Add(uname);
+            AddAdmin.Location = new Point(238, 112);
+            AddAdmin.Name = "AddAdmin";
+            AddAdmin.Size = new Size(636, 443);
+            AddAdmin.TabIndex = 13;
+            // 
+            // them
+            // 
+            them.BackColor = Color.Green;
+            them.Location = new Point(346, 351);
+            them.Name = "them";
+            them.Size = new Size(97, 38);
+            them.TabIndex = 10;
+            them.Text = "Thêm";
+            them.UseVisualStyleBackColor = false;
+            them.Click += them_Click;
+            // 
+            // tkemail
+            // 
+            tkemail.Location = new Point(133, 277);
+            tkemail.Name = "tkemail";
+            tkemail.Size = new Size(268, 27);
+            tkemail.TabIndex = 9;
+            // 
+            // email
+            // 
+            email.AutoSize = true;
+            email.Location = new Point(60, 277);
+            email.Name = "email";
+            email.Size = new Size(46, 20);
+            email.TabIndex = 8;
+            email.Text = "Email";
+            // 
+            // sodienthoai
+            // 
+            sodienthoai.Location = new Point(133, 218);
+            sodienthoai.Name = "sodienthoai";
+            sodienthoai.Size = new Size(268, 27);
+            sodienthoai.TabIndex = 7;
+            // 
+            // sdt
+            // 
+            sdt.AutoSize = true;
+            sdt.Location = new Point(13, 218);
+            sdt.Name = "sdt";
+            sdt.Size = new Size(97, 20);
+            sdt.TabIndex = 6;
+            sdt.Text = "Số điện thoại";
+            // 
+            // password
+            // 
+            password.Location = new Point(133, 158);
+            password.Name = "password";
+            password.Size = new Size(268, 27);
+            password.TabIndex = 5;
+            // 
+            // pass
+            // 
+            pass.AutoSize = true;
+            pass.Location = new Point(32, 158);
+            pass.Name = "pass";
+            pass.Size = new Size(70, 20);
+            pass.TabIndex = 4;
+            pass.Text = "mật khẩu";
+            // 
+            // hovaten
+            // 
+            hovaten.Location = new Point(133, 99);
+            hovaten.Name = "hovaten";
+            hovaten.Size = new Size(268, 27);
+            hovaten.TabIndex = 3;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.Location = new Point(32, 99);
+            name.Name = "name";
+            name.Size = new Size(73, 20);
+            name.TabIndex = 2;
+            name.Text = "Họ và tên";
+            // 
+            // username
+            // 
+            username.Location = new Point(133, 42);
+            username.Name = "username";
+            username.Size = new Size(268, 27);
+            username.TabIndex = 1;
+            // 
+            // uname
+            // 
+            uname.AutoSize = true;
+            uname.Location = new Point(32, 42);
+            uname.Name = "uname";
+            uname.Size = new Size(78, 20);
+            uname.TabIndex = 0;
+            uname.Text = "UserName";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.HotTrack;
+            label4.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label4.ForeColor = Color.Cornsilk;
+            label4.Location = new Point(0, 3);
+            label4.Name = "label4";
+            label4.Size = new Size(436, 41);
+            label4.TabIndex = 12;
+            label4.Text = "Thêm Tài khoản khách hàng";
+            // 
             // TTKhachHang
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -133,6 +271,10 @@
             TTSP.ResumeLayout(false);
             TTSP.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)TTCustomers).EndInit();
+            addKH.ResumeLayout(false);
+            addKH.PerformLayout();
+            AddAdmin.ResumeLayout(false);
+            AddAdmin.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -145,5 +287,18 @@
         private TabPage addKH;
         private TabPage delKH;
         private TabPage updateKH;
+        private Panel AddAdmin;
+        private Button them;
+        private TextBox tkemail;
+        private Label email;
+        private TextBox sodienthoai;
+        private Label sdt;
+        private TextBox password;
+        private Label pass;
+        private TextBox hovaten;
+        private Label name;
+        private TextBox username;
+        private Label uname;
+        private Label label4;
     }
 }
--- APP/APP/APP/TTKhachHang.cs
+++ APP/APP/APP/TTKhachHang.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,68 @@ namespace APP
 {
     public partial class TTKhachHang : Form
     {
+        static MySqlConnection conn;
         public TTKhachHang()
         {
             InitializeComponent();
+            Connection();
+        }
+        static void Connection()
+        {
+            string connstr = "server=127.0.0.1;uid=root;pwd=;database=boardgame";
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void TTCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void them_Click(object sender, EventArgs e)
+        {
+            string user = username.Text;
+            string passwd = password.Text;
+            string hoten = hovaten.Text;
+            string phone = sodienthoai.Text;
+            string mail = tkemail.Text;
+            conn.Open();
+            MySqlCommand mySqlCommand = new MySqlCommand("insert into Customers(UserName,HOTEN,PassWord,sdt,email) values(@user, @hoten, @passwd, @phone, @mail) ", conn);
+            mySqlCommand.Parameters.AddWithValue("@user", user);
+            mySqlCommand.Parameters.AddWithValue("@hoten", hoten);
+            mySqlCommand.Parameters.AddWithValue("@passwd", passwd);
+            mySqlCommand.Parameters.AddWithValue("@phone", phone);
+            mySqlCommand.Parameters.AddWithValue("@mail", mail);
+            mySqlCommand.ExecuteNonQuery();
+            DateTime currentDate = DateTime.Now;
+
+            string formattedDate = currentDate.ToString("yyyy-MM-dd");
+
+            MySqlCommand mySqlCommand1 = new MySqlCommand("insert into wp_users(User_login,display_name,user_nicename,user_Pass,user_email,user_registered) values(@user, @hoten, @user,@passwd, @mail,@date) ", conn);
+            mySqlCommand1.Parameters.AddWithValue("@user", user);
+            mySqlCommand1.Parameters.AddWithValue("@hoten", hoten);
+            mySqlCommand1.Parameters.AddWithValue("@passwd", passwd);
+            mySqlCommand1.Parameters.AddWithValue("@mail", mail);
+            mySqlCommand1.Parameters.AddWithValue("@date", formattedDate);
+            mySqlCommand1.ExecuteNonQuery();
+            MessageBox.Show("Thêm Khách hàng thành công!");
+            conn.Close();
+            username.Text = "";
+            hovaten.Text = "";
+            password.Text = "";
+            sodienthoai.Text = "";
+            tkemail.Text = "";
         }
     }
 }
