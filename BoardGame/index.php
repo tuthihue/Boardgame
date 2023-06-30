58e0d49eb129013d8b08603690389031193c36a9
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-06-30 23:06:58 +0700

    Trang chủ cho website

---
+++ BoardGame/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ | BoardGame</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+<div id="wrapper">
+        <div id="header">
+            <div class="container">
+                <div class="logo">
+                    <img src="logo-boardgame.png" alt="">
+                </div>
+                <div class="search-bar">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div id="menu">
+              <div class="container">
+              <div class="menu-area">
+                  <ul>
+                      <li><a href="/BoardGame/index.php">Trang chủ</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+          </div>
+      </div>
+
+        <!-- end header -->
+
+        <!-- end main -->
+        <div div id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-col">
+                            <h4>Thông tin</h4>
+                            <ul>
+                                <li><a href="/DoAnTKWeb">Trang chủ</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h4>Theo dõi chúng tôi</h4>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+         <!-- end foooter -->
+    </div>
+</body>
+
+</html>
