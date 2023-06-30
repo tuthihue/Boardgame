b567ba1e0e2478ee6a5a54180d2cf283525bb64a
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-06-30 23:33:44 +0700

    Chức năng đăng nhập admin

--- APP/APP/APP/LoginAdmin.Designer.cs
+++ APP/APP/APP/LoginAdmin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace APP
 {
-    partial class Form1
+    partial class LoginAdmin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Exit = new Button();
+            Login = new Button();
+            username = new Label();
+            password = new TextBox();
+            pass = new Label();
+            account = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // Exit
+            // 
+            Exit.BackColor = Color.RoyalBlue;
+            Exit.ForeColor = SystemColors.ControlLightLight;
+            Exit.Location = new Point(673, 499);
+            Exit.Name = "Exit";
+            Exit.Size = new Size(125, 73);
+            Exit.TabIndex = 14;
+            Exit.Text = "Thoát";
+            Exit.UseVisualStyleBackColor = false;
+            Exit.Click += Exit_Click;
+            // 
+            // Login
+            // 
+            Login.BackColor = Color.RoyalBlue;
+            Login.ForeColor = SystemColors.ControlLightLight;
+            Login.Location = new Point(433, 499);
+            Login.Name = "Login";
+            Login.Size = new Size(166, 73);
+            Login.TabIndex = 13;
+            Login.Text = "Đăng nhập";
+            Login.UseVisualStyleBackColor = false;
+            Login.Click += Login_Click;
+            // 
+            // username
+            // 
+            username.AutoSize = true;
+            username.BackColor = Color.Transparent;
+            username.Location = new Point(213, 356);
+            username.Name = "username";
+            username.Size = new Size(107, 20);
+            username.TabIndex = 9;
+            username.Text = "Tên đăng nhập";
+            username.Click += username_Click;
+            // 
+            // password
+            // 
+            password.Location = new Point(433, 418);
+            password.Name = "password";
+            password.Size = new Size(365, 27);
+            password.TabIndex = 12;
+            password.UseSystemPasswordChar = true;
+            password.TextChanged += password_TextChanged;
+            // 
+            // pass
+            // 
+            pass.AutoSize = true;
+            pass.BackColor = Color.Transparent;
+            pass.Location = new Point(213, 418);
+            pass.Name = "pass";
+            pass.Size = new Size(70, 20);
+            pass.TabIndex = 11;
+            pass.Text = "Mật khẩu";
+            pass.Click += pass_Click;
+            // 
+            // account
+            // 
+            account.Location = new Point(433, 356);
+            account.Name = "account";
+            account.Size = new Size(365, 27);
+            account.TabIndex = 10;
+            account.TextChanged += account_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(484, 95);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(223, 208);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1010, 667);
+            Controls.Add(Exit);
+            Controls.Add(Login);
+            Controls.Add(username);
+            Controls.Add(password);
+            Controls.Add(pass);
+            Controls.Add(account);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Exit;
+        private Button Login;
+        private Label username;
+        public TextBox password;
+        private Label pass;
+        public TextBox account;
+        private PictureBox pictureBox1;
     }
 }
--- APP/APP/APP/LoginAdmin.cs
+++ APP/APP/APP/LoginAdmin.cs
@@ -1,10 +1,60 @@
+using System;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace APP
 {
-    public partial class Form1 : Form
+    public partial class LoginAdmin : Form
     {
-        public Form1()
+        static MySqlConnection conn = null;
+        public string Account_Admin;
+        public LoginAdmin()
         {
             InitializeComponent();
+            Connection();
+        }
+        static void Connection()
+        {
+
+            string connstr = "server=127.0.0.1;uid=root;pwd=;database=boardgame";
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void username_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void pass_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void account_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
