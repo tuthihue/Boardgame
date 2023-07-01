984ae043d4359d0f04b8cc4c06f378588bce1eee
Tu Thi Hue <92870027+Smililyy@users.noreply.github.com>
2023-07-01 21:40:08 +0700

    Chỉnh sửa/ thêm thuộc tính thanh công cụ và footer của trang chủ

--- BoardGame/index.php
+++ BoardGame/index.php
@@ -8,29 +8,60 @@
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body>
-<div id="wrapper">
-        <div id="header">
-            <div class="container">
-                <div class="logo">
-                    <img src="logo-boardgame.png" alt="">
-                </div>
-                <div class="search-bar">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </div>
-            </div>
-            <div id="menu">
-              <div class="container">
-              <div class="menu-area">
-                  <ul>
-                      <li><a href="/BoardGame/index.php">Trang chủ</a></li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-          </div>
-      </div>
-
+<header>
+    <div class="logo">BOARDGAME</div>
+    <nav>
+      <ul>
+        <li><a href="index.php">Trang chủ</a></li>
+        <li><a href="product.php">Sản phẩm</a></li>
+        <li>
+          <a href="#">Thể loại</a>
+          <!-- Thêm danh sách cho mục "Thể loại" -->
+          <ul class="sub-menu">
+            <li><a href="#">Card Games</a></li> 
+            <li><a href="#">Dice Games</a></li> 
+            <li><a href="#">Chess</a></li> 
+            <li><a href="#">Family</a></li> 
+            <li><a href="#">Living Card Games</a></li> 
+            <li><a href="#">Deckbuilders</a></li> 
+            <li><a href="#">Wargame & Strategy</a></li> 
+            <li><a href="#">Cooperative</a></li> 
+            <li><a href="#">Dungeon Crawl & Minis</a></li> 
+            <li><a href="#">Party Games</a></li> 
+            <li><a href="#">Star Wars</a></li> 
+          </ul>
+        </li>
+        <li><a href="contact.php">Liên hệ</a></li>
+        <li><a href="account.php">Tài khoản</a></li>
+      </ul>
+    </nav>
+    <div class="search-bar">
+      <input type="text" placeholder="Tìm kiếm">
+      <button type="submit">Tìm kiếm</button>
+    </div>
+    <?php 
+    session_start();
+    $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
+    if ($username != "") { 
+?>
+<div class="account-icon">
+    <i class="fas fa-user"></i>
+    <span class="name">Xin chào, <?php echo $username ?></span>
+</div>
+<?php } else { ?>
+<a href="login.php">
+    <div class="account-icon">
+        <i class="fas fa-user"></i>
+    </div>
+</a>
+<?php } ?>
+    <div class="cart-icon">
+    <a href="cart.php">
+      <i class="fas fa-shopping-cart"></i>
+      <span class="cart-count">0</span>
+    </a>
+    </div>
+  </header>
         <!-- end header -->
         <div id="main">
             <div class="container">
@@ -373,7 +404,10 @@
                         <div class="footer-col">
                             <h4>Thông tin</h4>
                             <ul>
-                                <li><a href="/DoAnTKWeb">Trang chủ</a></li>
+                                <li><a href="contact.php">Liên hệ</a></li>
+                                <li><a href="policy.php">Chính sách</a></li>
+                                <li><a href="address.php">Địa chỉ</a></li>
+                                <li><a href="respond.php">Phản hồi</a></li>
                             </ul>
                         </div>
                         <div class="footer-col">
