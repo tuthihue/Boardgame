d12e3e40fd25d56d09b57d25ecf2a6093b3f1cdc
DESKTOP-DCB8O5U\hongp <hoangngochong03@gmail.com>
2023-07-21 12:50:41 +0700

    Sửa lỗi đăng nhập cho user

--- BoardGame/login_process.php
+++ BoardGame/login_process.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_array($result);
         session_start();
-        $_SESSION['username'] = $row['username'];
+        $_SESSION['username'] = $name;
         // Chuyển hướng người dùng đến trang index.php khi đăng nhập thành công
         header('location: index.php');
     } else {
